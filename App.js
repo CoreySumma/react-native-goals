ed9001a69a58d498a316322b8a8a36f61c95b3fb
CoreySumma <CoreySumma@gmail.com>
2023-02-01 15:00:23 -0600

    implimented flex for mobile display

--- App.js
+++ App.js
@@ -11,10 +11,10 @@ export default function App() {
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your Goal" />
+        <TextInput style={styles.textInput} placeholder="Enter Your Goal Here" />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -23,16 +23,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 60,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: '80%',
-    marginRight: 8
+    width: '70%',
+    marginRight: 8,
+    padding: 8,  
+  },
+  goalsContainer: {
+    flex: 3,
   }
 });
